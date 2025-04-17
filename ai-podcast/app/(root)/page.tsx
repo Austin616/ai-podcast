8ed9feb400c3d7e7fce516a39e0e36731f377a40
Austin Tran <austintran616@gmail.com>
2025-04-17 14:05:07 -0500

    renders added podcast, (no more temp data)

--- ai-podcast/app/(root)/page.tsx
+++ ai-podcast/app/(root)/page.tsx
@@ -7,24 +7,24 @@ import { api } from "@/convex/_generated/api";
 import { SignOutButton } from "@clerk/nextjs";
 
 const Home = () => {
-  // const tasks = useQuery(api.task.get);
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
         {/* <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+          
         </div> */}
 
         <div className="podcast_grid">
-          {podcastData.map(({ id, title, description, imgURL }) => (
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard
-              key={id}
-              podcastId={id}
-              title={title}
-              description={description}
-              imgURL={imgURL}
+              key={_id}
+              podcastId={parseInt(_id)}
+              title={podcastTitle}
+              description={podcastDescription}
+              imgURL={imageUrl!}
             />
           ))}
         </div>
